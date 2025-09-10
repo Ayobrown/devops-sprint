5ed966271ba27aab3e7907e04edd27e807c92e3f
Ayo Kayode <edwinkayodeayo@gmail.com>
2025-09-10 17:16:47 +0100

    made date display dynamic

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,39 @@
+(function ($) {
 
-  (function ($) {
-  
   "use strict";
 
-    // MENU
-    $('#sidebarMenu .nav-link').on('click',function(){
-      $("#sidebarMenu").collapse('hide');
-    });
-    
-    // CUSTOM LINK
-    $('.smoothscroll').click(function(){
-      var el = $(this).attr('href');
-      var elWrapped = $(el);
-      var header_height = $('.navbar').height();
-  
-      scrollToDiv(elWrapped,header_height);
-      return false;
-  
-      function scrollToDiv(element,navheight){
-        var offset = element.offset();
-        var offsetTop = offset.top;
-        var totalScroll = offsetTop-navheight;
-  
-        $('body,html').animate({
-        scrollTop: totalScroll
-        }, 300);
-      }
-    });
-  
-  })(window.jQuery);
+  // MENU
+  $('#sidebarMenu .nav-link').on('click', function () {
+    $("#sidebarMenu").collapse('hide');
+  });
 
+  // CUSTOM LINK
+  $('.smoothscroll').click(function () {
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height();
+
+    scrollToDiv(elWrapped, header_height);
+    return false;
+
+    function scrollToDiv(element, navheight) {
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop - navheight;
+
+      $('body,html').animate({
+        scrollTop: totalScroll
+      }, 300);
+    }
+  });
+
+  // DYNAMIC DEPLOY DATE
+  const today = new Date();
+  const formattedDate = today.toLocaleDateString('en-GB', {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric'
+  });
+  $('#deploy-date').text(formattedDate);
+
+})(window.jQuery);
